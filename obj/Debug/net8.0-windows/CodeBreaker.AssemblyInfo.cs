47525ed9037840835dc7eef40636e490cc18b775
Jeeru Visweswara <jvsks28@gmail.com>
2026-04-28 23:07:23 +0530

    inital commit

--- obj/Debug/net8.0-windows/CodeBreaker.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/CodeBreaker.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CodeBreaker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d78dba86d50a354228ea3de05b5912601f40087")]
 [assembly: System.Reflection.AssemblyProductAttribute("CodeBreaker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CodeBreaker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
